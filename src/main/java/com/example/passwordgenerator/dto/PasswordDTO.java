f215a2a88b8fe4869753c86e13945aa9a62ed82b
RafaelRAzevedo <r95@live.com.pt>
2020-05-01 05:10:45 +0000

    Created endpoint to create random password

---
+++ src/main/java/com/example/passwordgenerator/dto/PasswordDTO.java
@@ -0,0 +1,19 @@
+package com.example.passwordgenerator.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Data
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class PasswordDTO {
+
+    private boolean uppercase;
+    private boolean numbers;
+    private boolean special;
+    private int length;
+
+}
---
+++ src/main/java/com/example/passwordgenerator/rest/PasswordGeneratorRest.java
@@ -0,0 +1,45 @@
+package com.example.passwordgenerator.rest;
+
+import com.example.passwordgenerator.dto.PasswordDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+public class PasswordGeneratorRest {
+
+  @GetMapping("/generate")
+  public ResponseEntity<String> getPassword(@RequestBody @Valid PasswordDTO dto) {
+
+    String small = "abcdefghijklmnopqrstuvwxyz";
+    StringBuilder availableCharacters = new StringBuilder();
+
+    availableCharacters.append(small);
+
+    if (dto.isUppercase()) {
+      String uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      availableCharacters.append(uppercase);
+    }
+    if (dto.isNumbers()) {
+      String numbers = "0123456789";
+      availableCharacters.append(numbers);
+    }
+    if (dto.isSpecial()) {
+      String symbols = "!@#$%^&*_=+-/.?<>)";
+      availableCharacters.append(symbols);
+    }
+
+    StringBuilder password = new StringBuilder();
+
+    for(int i = 0; i < dto.getLength(); i++) {
+        int random = (int) (Math.random() * availableCharacters.length());
+        password.append(availableCharacters.charAt(random));
+    }
+
+    return new ResponseEntity<>(password.toString(), HttpStatus.OK);
+  }
+}
